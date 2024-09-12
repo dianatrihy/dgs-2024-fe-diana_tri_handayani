28d1c348f3e860e87b1a0113c8f589f907c93bda
dianatrihy <dianatrihy@gmail.com>
2024-09-12 22:00:26 +0700

    little bit ngerapiin (ngantuk)

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,10 +1,11 @@
 import CategoriesForm from '../components/CategoriesForm';
 import CategoriesList from '../components/CategoriesList';
+import './Categories.css'
 
 export default function Categories() {
     return (
-        <div>
-            <h3>Categories Page</h3>
+        <div className='categories'>
+            <h2>Categories Page</h2>
             <CategoriesForm />
             <CategoriesList />
         </div>
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,12 +1,13 @@
 import WalletForm from '../components/WalletForm';
 import WalletList from '../components/WalletList';
+import './Wallet.css'
 
 export default function Wallet() {
   return (
-    <>
-      <h3>Wallet List</h3>
+    <div className='wallet'>
+      <h2>Wallet List</h2>
       <WalletForm />
       <WalletList />
-    </>
+    </div>
   );
 }
